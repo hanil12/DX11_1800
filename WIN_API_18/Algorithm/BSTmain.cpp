795682ab004ceaf7ae5bc1678c5a60aed38bd411
hanil12 <ekdrhaod@gmail.com>
2022-11-16 22:02:24 +0900

    BST

--- WIN_API_18/Algorithm/BSTmain.cpp
+++ WIN_API_18/Algorithm/BSTmain.cpp
@@ -4,10 +4,19 @@
 #include <deque>
 #include <queue>
 #include <stack>
+#include <map>
 
 #include "BinarySearchTree.h"
 
 using namespace std;
+
+class Player
+{
+public:
+
+	int _id;
+	int _hp;
+};
 
 int main()
 {
@@ -22,6 +31,17 @@ int main()
 	tree.Insert(120);
 
 	tree.PrintTree(tree._root);
+
+	map<int, int> myMap;
+
+	myMap[120] = 0;
+	myMap[10] = 0;
+	myMap[56] = 0;
+	myMap[80] = 0;
+	myMap[2] = 0;
+	myMap[90] = 0;
+
+	myMap.insert(make_pair<int, int>(100, 1)); // pair first: Ű, second : data
 
 	return 0;
 }
--- WIN_API_18/Algorithm/BinarySearchTree.cpp
+++ WIN_API_18/Algorithm/BinarySearchTree.cpp
@@ -54,28 +54,121 @@ void BinarySearchTree::PrintTree(Node* root)
 
 Node* BinarySearchTree::Search(Node* root, int key)
 {
-	return nullptr;
+	// 기저사례
+	if (root == nullptr)
+		return root;
+
+	// 구하기
+	if (root->key == key)
+		return root;
+
+	if (root->key > key)
+	{
+		return Search(root->left, key);
+	}
+	else
+	{
+		return Search(root->right, key);
+	}
 }
 
 Node* BinarySearchTree::Min(Node* root)
 {
-	return nullptr;
+	//if (root == nullptr)
+	//	return root;
+
+	//if (root->left == nullptr)
+	//	return root;
+
+	//return Min(root->left);
+
+	if (root == nullptr)
+		return nullptr;
+
+	Node* node = root;
+	while (true)
+	{
+		if (node->left == nullptr)
+			return node;
+		node = node->left;
+	}
 }
 
 Node* BinarySearchTree::Max(Node* root)
 {
-	return nullptr;
+	// 첫 예외처리
+	if (root == nullptr)
+		return root;
+
+	// 기저사항
+	if (root->right == nullptr)
+		return root;
+
+	return Max(root->right);
 }
 
 Node* BinarySearchTree::Previous(Node* node)
 {
-	return nullptr;
+	if (node == nullptr)
+		return node;
+
+	if (node->left != nullptr)
+	{
+		Node* prev = Max(node->left);
+		if (prev == nullptr)
+			return node->left;
+		else
+			return prev;
+	}
+
+	Node* parent = node->parent;
+
+	while (parent != nullptr && node == parent->left)
+	{
+		node = parent;
+		parent = parent->parent;
+	}
+
+	return parent;
 }
 
 void BinarySearchTree::RePlace(Node* node1, Node* node2)
 {
+	// node1이 루트노드일 때
+	if (node1->parent == nullptr)
+		_root = node2;
+
+	// node1이 부모 쪽에서 left에 있는 경우
+	else if (node1 == node1->parent->left)
+	{
+		node1->parent->left = node2;
+	}
+
+	// node1이 부모쪽에서 right
+	else
+	{
+		node1->parent->right = node2;
+	}
+
+	if (node2)
+		node2->parent = node1->parent;
+
+	delete node1;
 }
 
 void BinarySearchTree::Delete(Node* node)
 {
+	if (node == nullptr)
+		return;
+
+	if (node->left == nullptr)
+		RePlace(node, node->right);
+	else if (node->right == nullptr)
+		RePlace(node, node->left);
+	else
+	{
+		Node* prev = Previous(node);
+		node->key = prev->key;
+		Delete(prev);
+	}
 }
